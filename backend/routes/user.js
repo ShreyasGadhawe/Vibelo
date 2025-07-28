ee5d2af5e311e19a392ffadf6bff78141dbd2adb
ShreyasGadhawe <shreyasgadhawe@gmail.com>
2025-07-28 19:23:46 +0530

    login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const pool = require('../db/db')
 const jwt = require('jsonwebtoken')
 const cryptoJs = require('crypto-js')
 const result = require('../utils/result')
-const secret = require('../utils/config')
+const {secret} = require('../utils/config')
 const router = express.Router()
 
 router.post("/register" , (request,response)=>{
@@ -12,6 +12,30 @@ router.post("/register" , (request,response)=>{
     const sql = `insert into users(name , phone_number , email , password , age , gender_id) values(?,?,?,?,?,?)`
     pool.query(sql , [name , phoneNo , email , encryptedPassword , age , gender],(error,data)=>{
         response.send(result.createResult(error,data))
+    })
+})
+
+router.post("/login", (request,response)=>{
+    const {email , password} = request.body
+    const encryptedPassword = String(cryptoJs.SHA256(password))
+    const sql = `select * from users where email=? and password=?`
+    pool.query(sql,[email , encryptedPassword],(error,data)=>{
+        if(data){
+            if(data.length != 0){
+                const payload = {
+                    userId : data[0].userId
+                }
+                const token = jwt.sign(payload,secret)
+                const body = {
+                    token : token,
+                    userName : data[0].name,
+                    userEmail : data[0].email
+                }
+                response.send(result.createSuccessResult(body))
+            }
+        }else{
+            response.send(result.createErrorResult(error))
+        }
     })
 })
 
